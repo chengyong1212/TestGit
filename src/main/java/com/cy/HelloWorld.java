fd42cdae2e132b218731bbfef5dfd8716e613de3
chengyong1212 <532716636@qq.com>
2018-06-29 17:25:52 +0800

    test33

---
+++ src/main/java/com/cy/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.cy;
+
+/**
+ * Created by chengyong on 2018/6/29.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
