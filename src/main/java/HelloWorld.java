e30a4cb1a2e03ae4583ed8e258e6e46a11dbd6f6
chengyong1212 <532716636@qq.com>
2021-05-30 18:46:54 +0800

    测试git代码提交

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld github");
+        System.out.println("HelloWorld github —— 20210530");
     }
     
 }
